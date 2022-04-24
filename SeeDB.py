315376820d1280f0c74cbb8a76ab4a9cd2834073
krutika23 <krutikasrd2306@gmail.com>
2022-04-24 18:00:38 -0400

    Final code

--- SeeDB.py
+++ SeeDB.py
@@ -12,17 +12,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from SeeDBUtils import SeeDB
-# import SeeDBUtils
 
-
-
-
-# psql -h cs645db.cs.umass.edu -p 7645
-                       
-#You are connected to database "ksarode" as user "ksarode" on host "cs645db.cs.umass.edu" (address "128.119.243.149") at port "7645".
-#SSL connection (protocol: TLSv1.3, cipher: TLS_AES_256_GCM_SHA384, bits: 256, compression: off)
-
-# conn = psycopg2.connect(dbname="ksarode", port="7645" , host="cs645db.cs.umass.edu")
 conn = psycopg2.connect(
     host="localhost",
     database="postgres",
@@ -31,49 +21,20 @@ conn = psycopg2.connect(
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
-#Executing an MYSQL function using the execute() method
-cursor.execute("select version()")
-
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-
-print(data)
 
 select_data_query = "select * from census"
 
 cursor.execute(select_data_query) 
 concensus_records = cursor.fetchall()
 
-
-        
-        
-# raw_df=pd.DataFrame(concensus_records,columns =['age', 'workclass','fnlwgt','education','education_num','marital_status','occupation','relationship','race','sex','capital_gain','capital_loss','hours_per_week','native_country','salary_range'])
-
-# q="select * from census where marital_status like 'Married%'"
-# cursor.execute(select_data_query) 
-# qr_df=pd.DataFrame(qr_records,columns =['age', 'workclass','fnlwgt','education','education_num','marital_status','occupation','relationship','race','sex','capital_gain','capital_loss','hours_per_week','native_country','salary_range'])
-
-
-# q="select * from census where marital_status not like 'Married%'"
-
-# cursor.execute(select_data_query) 
-# qd_records = cursor.fetchall()
-# qd_df=pd.DataFrame(qd_records,columns =['age', 'workclass','fnlwgt','education','education_num','marital_status','occupation','relationship','race','sex','capital_gain','capital_loss','hours_per_week','native_country','salary_range'])
-
-#
-
 q="drop view reference_data cascade;drop view actual_data cascade;create view reference_data as select * from census where marital_status not like 'Married%';create view actual_data as select * from census where marital_status like 'Married%';"
     
 cursor.execute(q) 
 conn.commit()
-      
-# cursor.execute("ROLLBACK")
-# conn.commit()
+
 
 num_phases=10
-# print(len(concensus_records))
 records_per_phase=int(len(concensus_records)/num_phases)
-# print(concensus_records[-1][0])
 div=np.linspace(0, len(concensus_records)-1, num_phases+1,dtype=int)
 # print(div)
  
@@ -81,57 +42,30 @@ see_db=SeeDB()
 sorted_dict=[]
 for i in range(0,num_phases):#len(div)-1):
     
-    # print(div[i],div[i+1])
     kl_divergence_dic=see_db.sharing_optimization_modified(cursor,div[i],div[i+1])
-    # print(kl_divergence_dic)
     conn.commit()
-    print(len(see_db.prunning_dict.keys()))
-    ret=see_db.prunning(cursor,kl_divergence_dic,i+1,num_phases)
-    print("Prunning list")
-    see_db.cleaning()
-    # print(see_db.prunning_dict)
     # print(len(see_db.prunning_dict.keys()))
+    ret=see_db.prunning(cursor,kl_divergence_dic,i+1,num_phases)
+    see_db.cleaning()
+    # print("length of deleted dict: ",len(see_db.deleted_attr))
+    # print("length of prunning dict: ",len(see_db.prunning_dict))
     sorted_dict=dict(sorted(see_db.prunning_dict.items(), key=lambda item: item[1][-1], reverse=True))
-    print(sorted_dict)
-    # top_k_values=[k for k,v in sorted_dict.items()][]
+    
     # print(top_k_values) 
     # see_db.plot(top_k_values[0])
-
-# sorted_dict=['education-fnlwgt-avg', 'workclass-fnlwgt-avg', 'relationship-education_num-avg', 'relationship-fnlwgt-avg', 'native_country-fnlwgt-avg']
-
-# sorted_dict=['relationship-hours_per_week-avg', 'relationship-fnlwgt-avg', 'race-hours_per_week-avg', 'race-fnlwgt-avg', 'relationship-hours_per_week-sum']
-
-for r in sorted_dict:
+print("Length:",sorted_dict)
+top_k_values=[k for k,v in sorted_dict.items()]
+print(top_k_values)
+print(len(top_k_values))
+for r in top_k_values[:10]:
     see_db.plot(r,cursor)
-# see_db.plot(sorted_dict[4],cursor)
-# conn.commit()
+# see_db.plot(sorted_dict,cursor)
+conn.commit()
 
 
     
     
 
-    
-# see_db=SeeDB()   
-# kl_divergence_dic=see_db.sharing_optimization_modified(cursor)
-# print(kl_divergence_dic)
-# conn.commit()
-
-# top5_pairs=kl_divergence_dic[0:5]
-# print(top5_pairs)
-    
-# for v in top5_pairs:
-#     plot(v[0])
-    
-# plot(top5_pairs[1][0])
-    
-            
-            
-            
-            
-            
-     
-    
-# kl_divergence([i[1] for i in sorted(workclass_reference_records)],[i[1] for i in sorted(workclass_actual_records)])
     
     
 
--- SeeDBUtils.py
+++ SeeDBUtils.py
@@ -29,46 +29,25 @@ class SeeDB():
 
     def kl_divergence(self,ref_data,actual_data):
         ref_data=self.normalize_data(ref_data)
-        # print("Normalized reference data: ",ref_data)
         actual_data=self.normalize_data(actual_data)
-        # print("Normalized actual data: ",actual_data)
         p_ref_data=[round(i/sum(ref_data),3) for i in ref_data] if sum(ref_data)>0 else ref_data
-        # print("probability of reference data: ",p_ref_data)
         p_act_data=[round(i/sum(actual_data),3) for i in actual_data] if sum(actual_data)>0 else actual_data
-        # print("probability of actual  data: ",p_act_data)
         return sum([(float(p_act_data[i])*(math.log(p_act_data[i]/p_ref_data[i]))) for i in range(len(p_ref_data)) if p_ref_data[i]>0 and p_act_data[i]>0])
 
-    # def _make_view_query( selections, table_name, cond, attribute,
-    #             start, end):
-    #         return ' '.join(['with attrs as (select distinct('
-    #                             + attribute + ') as __atr__',
-    #                             'from', table_name, ')',
-    #                         'select', selections,
-    #                         'from', 'attrs left outer join', table_name,
-    #                             'on', '__atr__ =', attribute,
-    #                             'and', cond,
-    #                             'and id>=' + str(start), 'and', 'id<' + str(end),
-    #                         'group by __atr__',
-    #                         'order by __atr__'])
-        
-    # res=_make_view_query( "colease", "actual_data","a<10", "marital_status", 5, 10)
+
     
-    def column_class_identification(self,cursor):
-        query="select * from actual_data"
-        cursor.execute(query)
-        records=cursor.fetchall()
-        df=pd.DataFrame(records,columns =['id','age', 'workclass','fnlwgt','education','education_num','marital_status','occupation','relationship','race','sex','capital_gain','capital_loss','hours_per_week','native_country','salary_range'])
-        for c in df.columns():
-            print(type(c))
+    # def column_class_identification(self,cursor):
+    #     query="select * from actual_data"
+    #     cursor.execute(query)
+    #     records=cursor.fetchall()
+    #     df=pd.DataFrame(records,columns =['id','age', 'workclass','fnlwgt','education','education_num','marital_status','occupation','relationship','race','sex','capital_gain','capital_loss','hours_per_week','native_country','salary_range'])
+    #     for c in df.columns():
+    #         print(type(c))
         
     def sharing_optimization(self,cursor):
-        # age | workclass | fnlwgt |  education   | education_num |   marital_status   |    occupation    | relationship  | race  | sex  | capital_gain | capital_loss | hours_per_week | native_country | salaray_range
         groupby_col,aggregate_col=self.column_class_identification(cursor)
         groupby_col=['workclass','education','occupation','relationship','race','sex','native_country']
-        # groupby_col=['workclass','education']
         aggregate_col=['fnlwgt','education_num','capital_gain','capital_loss','hours_per_week']
-        # aggregate_col=['capital_gain']
-        # group_by=['sum','count','avg']
         kl_divergence_dict={}
         for gb_col in groupby_col:
             for agg_col in aggregate_col:
@@ -89,7 +68,6 @@ class SeeDB():
 
                         
                 qa="select {}, {}({}),{}({}),{}({}) from actual_data group by {}".format(gb_col,"sum",agg_col,"count",agg_col,"avg",agg_col,gb_col)
-                print(qa)
                 cursor.execute(qa) 
                 actual_records = cursor.fetchall()
                 for row in actual_records:
@@ -97,7 +75,6 @@ class SeeDB():
                         act_cnt[row[0]]=row[2]
                         act_avg[row[0]]=row[3]
 
-                print("For testing: ",gb_col,"-",agg_col,[v for k,v in ref_sum.items() if k in ref_sum and k in act_sum ])
 
         
     def sharing_optimization_modified(self,cursor,start,end):
@@ -156,12 +133,12 @@ class SeeDB():
                 lst.append(self.kl_divergence([v for k,v in sorted(ref_sum.items()) if k in ref_sum and k in act_sum ],[v for k,v in sorted(act_sum.items()) if k in ref_sum and k in act_sum ]))
                 self.prunning_dict[gb_col+"-"+agg_col+"-"+"sum"]=lst
                 
-            if gb_col+"-"+agg_col+"-"+"cnt" in self.prunning_dict.keys() and gb_col+"-"+agg_col+"-"+"cnt" not in self.deleted_attr:
-                self.prunning_dict[gb_col+"-"+agg_col+"-"+"cnt"].append(self.kl_divergence([v for k,v in sorted(ref_cnt.items()) if k in ref_cnt and k in act_cnt ],[v for k,v in sorted(act_cnt.items()) if k in ref_cnt and k in act_cnt ]))
-            elif gb_col+"-"+agg_col+"-"+"cnt" not in self.deleted_attr:
+            if gb_col+"-"+agg_col+"-"+"count" in self.prunning_dict.keys() and gb_col+"-"+agg_col+"-"+"count" not in self.deleted_attr:
+                self.prunning_dict[gb_col+"-"+agg_col+"-"+"count"].append(self.kl_divergence([v for k,v in sorted(ref_cnt.items()) if k in ref_cnt and k in act_cnt ],[v for k,v in sorted(act_cnt.items()) if k in ref_cnt and k in act_cnt ]))
+            elif gb_col+"-"+agg_col+"-"+"count" not in self.deleted_attr:
                 lst=[]
                 lst.append(self.kl_divergence([v for k,v in sorted(ref_cnt.items()) if k in ref_cnt and k in act_cnt ],[v for k,v in sorted(act_cnt.items()) if k in ref_cnt and k in act_cnt ]))
-                self.prunning_dict[gb_col+"-"+agg_col+"-"+"cnt"]=lst
+                self.prunning_dict[gb_col+"-"+agg_col+"-"+"count"]=lst
                 
             if gb_col+"-"+agg_col+"-"+"avg" in self.prunning_dict.keys() and gb_col+"-"+agg_col+"-"+"avg" not in self.deleted_attr:
                 self.prunning_dict[gb_col+"-"+agg_col+"-"+"avg"].append(self.kl_divergence([v for k,v in sorted(ref_avg.items()) if k in ref_avg and k in act_avg ],[v for k,v in sorted(act_avg.items()) if k in ref_avg and k in act_avg ]))
@@ -174,33 +151,29 @@ class SeeDB():
         
         for k,v in self.prunning_dict.items():
             # import pdb;pdb.set_trace()
-            print(k,v)
             delta=0.05
             numerator = (1 - ((phase_num - 1)/adi))*(2*np.log(np.log(phase_num)) + np.log((np.pi*np.pi) / (3*delta)))
             epsilon_m = np.sqrt(numerator / (2 * phase_num))
             # epsilon_m = self.get_confidence(len(v), phase_num, 0.05)
             mean_utility_measure=sum(v)/phase_num
-            #std_err = scipy.stats.sem(np.array(v))
-            #h = std_err * scipy.stats.t.ppf((1 + confidence) / 2., len(v) - 1)
-            # import pdb;pdb.set_trace()
             confidence_intervals[k] = [mean_utility_measure-epsilon_m, mean_utility_measure+epsilon_m]
         # return confidence_intervals
         for kk,v in confidence_intervals.items():
             count=0
             # import pdb;pdb.set_trace()
             for key,value in confidence_intervals.items():
+                # print(confidence_intervals[kk][1] ,"and", confidence_intervals[key][0])
                 if confidence_intervals[kk][1] < confidence_intervals[key][0]:
                     count += 1
-                if count >= adi:
+                if count >= 3:
+                    print("breaking point")
                     break
-            if count>=adi:
+            if count>=3:
                 self.deleted_attr.append(kk)
-        # print(self.deleter_attr)
         return self.deleted_attr
     
     def get_confidence(self, m, N, delta):
         t1=1-((m-1)/N)
-        print(m)
         m=abs(m)
         t2=2*math.log(math.log(m))
         t3=math.log(math.pi**2/(3*delta))
@@ -208,16 +181,6 @@ class SeeDB():
         denominator=2*m
         epsilon_m=(numerator/denominator)**0.5
         return epsilon_m
-
-    # def update_stats(dict_kl):
-    #     dict_stats=defaultdict(dict)
-    #     for key,vals in dict_kl.items():
-    #         dict_stats[key]['mean']=sum(vals)/len(vals)
-    #         CI = get_confidence(len(dict_kl[key]), num_partitions, delta)
-    #         dict_stats[key]['lower_CI'] = dict_stats[key]['mean'] - CI
-    #         dict_stats[key]['upper_CI'] = dict_stats[key]['mean'] + CI
-    #     return dict_stats
-                
 
         
                             
@@ -235,7 +198,6 @@ class SeeDB():
         ref_query="select {}, {}({}) from reference_data group by {}".format(a,fm,m,a)
         cursor.execute(ref_query) 
         ref_records = cursor.fetchall()
-        print("Print each row and it's columns values")
         ref_index=[]
         ref_data=[]
         for row in ref_records:
@@ -243,15 +205,13 @@ class SeeDB():
         act_query="select {}, {}({}) from actual_data group by {}".format(a,fm,m,a)
         cursor.execute(act_query) 
         act_records = cursor.fetchall()
-        print("Print each row and it's columns values")
         act_index=[]
         act_data=[]
         for row in act_records:
             act_index.append(row[0])
         n_groups = len(act_index)
         groups=list(set(act_index).intersection(set(ref_index)))
-        print(groups)
-        print(len(groups))
+
         for row in ref_records:
             if row[0] in groups:
                 ref_data.append(row[1])
@@ -263,41 +223,22 @@ class SeeDB():
         index = np.arange(n_groups)
         
         X = groups
-        print("X: ",groups)
         Ygirls = act_data
-        print("Act data: ",act_data)
         Zboys = ref_data
-        print("Ref data: ",ref_data)
-        print(len(X)," ",len(act_data)," ",len(ref_data))
+
           
         X_axis = np.arange(len(X))
           
         plt.bar(X_axis - 0.2, Ygirls, 0.4, label = 'Married')
         plt.bar(X_axis + 0.2, Zboys, 0.4, label = 'NotMarried')
-          
+        fig = plt.figure(figsize=(10, 10))
+        # fig.autofmt_xdate()
         plt.xticks(X_axis, X)
         plt.xlabel(a)
         plt.ylabel(m)
         plt.title("Number of {} in each {}".format(m,a))
         plt.legend()
         plt.show()
-        print("plotted data")
 
 
-        # bar_width = 0.35
-        # opacity = 0.8
-        # rects1 = plt.bar(index, act_data, bar_width,
-        #                  alpha=opacity,
-        #                  color='b',
-        #                  label='Married')
-        # rects2 = plt.bar(index + bar_width, ref_data, bar_width,
-        #                  alpha=opacity,
-        #                  color='g',
-        #                  label='Unmarried')
-        # plt.xlabel(a)
-        # plt.ylabel('{}({})'.format(fm,m))
-        # plt.title('{} by {} for married and unmarried.'.format(m,a))
-        # plt.xticks(index + bar_width,act_index,rotation=45,horizontalalignment="right")
-        # plt.legend()
-        # plt.tight_layout()
         
